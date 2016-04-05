095ce73db232fabaa8779c8b9cddbd8aa9cc6925
Corentin <lecomte.corentin@gmail.com>
2016-04-05 16:18:50 +0200

    add require

--- index.php
+++ index.php
@@ -8,6 +8,8 @@
 
 
 
+require "vendor/autoload.php"
+
 // Set your secret key: remember to change this to your live secret key in production
 // See your keys here https://dashboard.stripe.com/account/apikeys
 \Stripe\Stripe::setApiKey(" sk_test_oeqZpwXuDXdpyPSnwvUWs96m");
